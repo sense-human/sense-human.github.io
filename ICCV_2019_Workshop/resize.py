1c2b8b7d43ece456114eef3c84c8e6b07239f13d
penincillin <rongyu9124@hotmail.com>
2019-05-31 15:55:19 +0800

    update

--- ICCV_2019_Workshop/resize.py
+++ ICCV_2019_Workshop/resize.py
@@ -1,4 +1,4 @@
 import cv2
-img = cv2.imread('angjoo-kanazawa.png')
-new_img = img[100:1100, 250:1250, :]
-cv2.imwrite('new.jpg', new_img)
+img = cv2.imread('tinghui-zhou.png')
+new_img = img[20:800, :, :]
+cv2.imwrite('new.png', new_img)
